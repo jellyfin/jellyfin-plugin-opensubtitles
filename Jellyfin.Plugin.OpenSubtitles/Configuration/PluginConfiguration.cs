541d3818e5aa0da53fa13c6dc88e667693c7f461
crobibero <cody@robibe.ro>
2021-03-14 08:18:34 -0600

    Add analyzers

--- Jellyfin.Plugin.OpenSubtitles/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.OpenSubtitles/Configuration/PluginConfiguration.cs
@@ -2,10 +2,19 @@ using MediaBrowser.Model.Plugins;
 
 namespace Jellyfin.Plugin.OpenSubtitles.Configuration
 {
+    /// <summary>
+    /// The plugin configuration.
+    /// </summary>
     public class PluginConfiguration : BasePluginConfiguration
     {
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
         public string Username { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
         public string Password { get; set; } = string.Empty;
     }
 }
---
+++ Jellyfin.Plugin.OpenSubtitles/OpenApiException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Jellyfin.Plugin.OpenSubtitles
+{
+    /// <summary>
+    /// Open api exception.
+    /// </summary>
+    public class OpenApiException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenApiException"/> class.
+        /// </summary>
+        /// <param name="msg">The exception message.</param>
+        public OpenApiException(string msg)
+            : base(msg)
+        {
+        }
+    }
+}
