1ae4798ca4e274520b30cc0d0d843428efbf70bc
MBR-0001 <55142207+MBR-0001@users.noreply.github.com>
2024-04-03 23:32:56 +0200

    10.9 support (#115)

--- Jellyfin.Plugin.OpenSubtitles/OpenSubtitlesHandler/Models/Responses/SearchResult.cs
+++ Jellyfin.Plugin.OpenSubtitles/OpenSubtitlesHandler/Models/Responses/SearchResult.cs
@@ -2,29 +2,28 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace Jellyfin.Plugin.OpenSubtitles.OpenSubtitlesHandler.Models.Responses
+namespace Jellyfin.Plugin.OpenSubtitles.OpenSubtitlesHandler.Models.Responses;
+
+/// <summary>
+/// The search result.
+/// </summary>
+public class SearchResult
 {
     /// <summary>
-    /// The search result.
+    /// Gets or sets the total page count.
     /// </summary>
-    public class SearchResult
-    {
-        /// <summary>
-        /// Gets or sets the total page count.
-        /// </summary>
-        [JsonPropertyName("total_pages")]
-        public int TotalPages { get; set; }
+    [JsonPropertyName("total_pages")]
+    public int TotalPages { get; set; }
 
-        /// <summary>
-        /// Gets or sets the current page.
-        /// </summary>
-        [JsonPropertyName("page")]
-        public int Page { get; set; }
+    /// <summary>
+    /// Gets or sets the current page.
+    /// </summary>
+    [JsonPropertyName("page")]
+    public int Page { get; set; }
 
-        /// <summary>
-        /// Gets or sets the list of response data.
-        /// </summary>
-        [JsonPropertyName("data")]
-        public IReadOnlyList<ResponseData> Data { get; set; } = Array.Empty<ResponseData>();
-    }
+    /// <summary>
+    /// Gets or sets the list of response data.
+    /// </summary>
+    [JsonPropertyName("data")]
+    public IReadOnlyList<ResponseData> Data { get; set; } = Array.Empty<ResponseData>();
 }
--- Jellyfin.Plugin.OpenSubtitles/OpenSubtitlesHandler/OpenSubtitlesRequestHelper.cs
+++ Jellyfin.Plugin.OpenSubtitles/OpenSubtitlesHandler/OpenSubtitlesRequestHelper.cs
@@ -10,105 +10,104 @@ using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Jellyfin.Plugin.OpenSubtitles.OpenSubtitlesHandler
+namespace Jellyfin.Plugin.OpenSubtitles.OpenSubtitlesHandler;
+
+/// <summary>
+/// Http util helper.
+/// </summary>
+public class OpenSubtitlesRequestHelper
 {
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly string _version;
+
     /// <summary>
-    /// Http util helper.
+    /// Initializes a new instance of the <see cref="OpenSubtitlesRequestHelper"/> class.
     /// </summary>
-    public class OpenSubtitlesRequestHelper
+    /// <param name="factory">Instance of the <see cref="IHttpClientFactory"/> interface.</param>
+    /// <param name="version">The plugin version.</param>
+    public OpenSubtitlesRequestHelper(IHttpClientFactory factory, string version)
     {
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly string _version;
+        _clientFactory = factory;
+        _version = version;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="OpenSubtitlesRequestHelper"/> class.
-        /// </summary>
-        /// <param name="factory">Instance of the <see cref="IHttpClientFactory"/> interface.</param>
-        /// <param name="version">The plugin version.</param>
-        public OpenSubtitlesRequestHelper(IHttpClientFactory factory, string version)
+    /// <summary>
+    /// Gets or sets the current instance.
+    /// </summary>
+    public static OpenSubtitlesRequestHelper? Instance { get; set; }
+
+    /// <summary>
+    /// Calculates: size + 64bit chksum of the first and last 64k (even if they overlap because the file is smaller than 128k).
+    /// </summary>
+    /// <param name="input">The input stream.</param>
+    /// <returns>The hash as Hexadecimal string.</returns>
+    public static string ComputeHash(Stream input)
+    {
+        const int HashLength = 8; // 64 bit hash
+        const long HashPos = 64 * 1024; // 64k
+
+        long streamsize = input.Length;
+        ulong hash = (ulong)streamsize;
+
+        Span<byte> buffer = stackalloc byte[HashLength];
+        while (input.Position < HashPos && input.Read(buffer) > 0)
         {
-            _clientFactory = factory;
-            _version = version;
+            hash += BinaryPrimitives.ReadUInt64LittleEndian(buffer);
         }
 
-        /// <summary>
-        /// Gets or sets the current instance.
-        /// </summary>
-        public static OpenSubtitlesRequestHelper? Instance { get; set; }
-
-        /// <summary>
-        /// Calculates: size + 64bit chksum of the first and last 64k (even if they overlap because the file is smaller than 128k).
-        /// </summary>
-        /// <param name="input">The input stream.</param>
-        /// <returns>The hash as Hexadecimal string.</returns>
-        public static string ComputeHash(Stream input)
+        input.Seek(-HashPos, SeekOrigin.End);
+        while (input.Read(buffer) > 0)
         {
-            const int HashLength = 8; // 64 bit hash
-            const long HashPos = 64 * 1024; // 64k
-
-            long streamsize = input.Length;
-            ulong hash = (ulong)streamsize;
-
-            Span<byte> buffer = stackalloc byte[HashLength];
-            while (input.Position < HashPos && input.Read(buffer) > 0)
-            {
-                hash += BinaryPrimitives.ReadUInt64LittleEndian(buffer);
-            }
-
-            input.Seek(-HashPos, SeekOrigin.End);
-            while (input.Read(buffer) > 0)
-            {
-                hash += BinaryPrimitives.ReadUInt64LittleEndian(buffer);
-            }
-
-            BinaryPrimitives.WriteUInt64BigEndian(buffer, hash);
-            return Convert.ToHexString(buffer).ToLowerInvariant();
+            hash += BinaryPrimitives.ReadUInt64LittleEndian(buffer);
         }
 
-        internal async Task<(string body, Dictionary<string, string> headers, HttpStatusCode statusCode)> SendRequestAsync(
-            string url,
-            HttpMethod method,
-            object? body,
-            Dictionary<string, string> headers,
-            CancellationToken cancellationToken)
+        BinaryPrimitives.WriteUInt64BigEndian(buffer, hash);
+        return Convert.ToHexString(buffer).ToLowerInvariant();
+    }
+
+    internal async Task<(string body, Dictionary<string, string> headers, HttpStatusCode statusCode)> SendRequestAsync(
+        string url,
+        HttpMethod method,
+        object? body,
+        Dictionary<string, string> headers,
+        CancellationToken cancellationToken)
+    {
+        var client = _clientFactory.CreateClient("Default");
+
+        HttpContent? content = null;
+        if (method != HttpMethod.Get && body is not null)
         {
-            var client = _clientFactory.CreateClient("Default");
-
-            HttpContent? content = null;
-            if (method != HttpMethod.Get && body != null)
-            {
-                content = JsonContent.Create(body);
-            }
-
-            using var request = new HttpRequestMessage
-            {
-                Method = method,
-                RequestUri = new Uri(url),
-                Content = content,
-                Headers =
-                {
-                    UserAgent = { new ProductInfoHeaderValue("Jellyfin-Plugin-OpenSubtitles", _version) },
-                    Accept = { new MediaTypeWithQualityHeaderValue("*/*") }
-                }
-            };
-
-            foreach (var (key, value) in headers)
-            {
-                if (string.Equals(key, "authorization", StringComparison.OrdinalIgnoreCase))
-                {
-                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", value);
-                }
-                else
-                {
-                    request.Headers.Add(key, value);
-                }
-            }
-
-            var result = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
-            var resHeaders = result.Headers.ToDictionary(x => x.Key.ToLowerInvariant(), x => x.Value.First());
-            var resBody = await result.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
-
-            return (resBody, resHeaders, result.StatusCode);
+            content = JsonContent.Create(body);
         }
+
+        using var request = new HttpRequestMessage
+        {
+            Method = method,
+            RequestUri = new Uri(url),
+            Content = content,
+            Headers =
+            {
+                UserAgent = { new ProductInfoHeaderValue("Jellyfin-Plugin-OpenSubtitles", _version) },
+                Accept = { new MediaTypeWithQualityHeaderValue("*/*") },
+            }
+        };
+
+        foreach (var (key, value) in headers)
+        {
+            if (string.Equals(key, "authorization", StringComparison.OrdinalIgnoreCase))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", value);
+            }
+            else
+            {
+                request.Headers.Add(key, value);
+            }
+        }
+
+        var result = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        var resHeaders = result.Headers.ToDictionary(x => x.Key.ToLowerInvariant(), x => x.Value.First());
+        var resBody = await result.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+
+        return (resBody, resHeaders, result.StatusCode);
     }
 }
